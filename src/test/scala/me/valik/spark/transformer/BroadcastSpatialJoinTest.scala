8584ff25df883d2ad2774e4271ab68eedc5b5882
Fedulov, Valentin <vasnake@gmail.com>
2019-04-17 18:53:27 +0300

    refactored tests

--- src/test/scala/me/valik/spark/transformer/BroadcastSpatialJoinTest.scala
+++ src/test/scala/me/valik/spark/transformer/BroadcastSpatialJoinTest.scala
@@ -3,7 +3,6 @@ package me.valik.spark.transformer
 import org.scalatest._
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import org.apache.spark.sql.{DataFrame, Row}
-import me.valik.spark.test._
 import org.apache.spark.storage.StorageLevel
 
 object BroadcastSpatialJoinTest {
@@ -69,33 +68,7 @@ class BroadcastSpatialJoinTest extends
 
     val transformer = makeTransformer(data.toDF)
     val output = transformer.transform(input) // .persist(StorageLevel.MEMORY_ONLY)
-    output.show(3, truncate=false)
+    //output.show(3, truncate=false)
     assertDataFrameEquals(output, expected.toDF)
-  }
-}
-
-class ContainmentJoinDataFrameTest extends
-  FlatSpec with Matchers with SimpleLocalSpark with DataFrameTestTools {
-
-  def makeTransformer: BroadcastSpatialJoin =
-    new BroadcastSpatialJoin()
-      .setDataset("poi")
-      .setDatasetPoint("lon, lat")
-
-  // testOnly me.valik.spark.transformer.ContainmentJoinTest -- -z "smoke"
-  it should "pass smoke test" in {
-    import spark.implicits._
-
-    val input = Seq(("1", "2019-04-05", "37.6095", "55.9297")).toDF("id", "date", "lon", "lat")
-    val expected = Seq(("1", "foo"), ("2", "bar")).toDF("id", "data")
-
-    val res = makeTransformer.transform(input)
-    // res.show(3, truncate=false)
-    res.collect should contain theSameElementsAs Seq(
-      Row("1", "foo"),
-      Row("2", "bar")
-    )
-
-    assertDataFrameEquals(res, expected)
   }
 }
---
+++ src/test/scala/me/valik/spark/transformer/BroadcastSpatialJoinTestWithCustomSpark.scala
@@ -0,0 +1,44 @@
+package me.valik.spark.transformer
+
+import org.scalatest._
+import org.apache.spark.sql.{DataFrame, Row}
+import me.valik.spark.test._
+
+class BroadcastSpatialJoinTestWithCustomSpark extends
+  FlatSpec with Matchers with SimpleLocalSpark with DataFrameTestTools {
+
+  import BroadcastSpatialJoinTest._
+
+  // testOnly me.valik.spark.transformer.BroadcastSpatialJoinTestWithCustomSpark -- -z "smoke"
+  it should "pass smoke test" in {
+    import spark.implicits._
+
+    val input = parsePointID(
+      """
+        |i1, 1, 1
+        |i2, 2, 2
+      """.stripMargin).toDS
+
+    val data = parsePoiID(
+      """
+        |d1, 1.1, 1.1
+        |d2, 2.1, 2.1
+      """.stripMargin).toDS
+
+    val expected = parsePointPoi(
+      """
+        |i1, 1, 1, d1
+        |i2, 2, 2, d2
+      """.stripMargin).toDS
+
+    val res = makeTransformer(data.toDF).transform(input)
+    //res.show(3, truncate=false)
+
+    //res.collect should contain theSameElementsAs Seq(
+    //  Row("i1", 1d, 1d, "d1"),
+    //  Row("i2", 2d, 2d, "d2")
+    //)
+
+    assertDataFrameEquals(res, expected.toDF)
+  }
+}
