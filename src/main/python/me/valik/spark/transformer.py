364e19ca2a81ecccb6f5606cdd80277fd5aaa8b0
Fedulov, Valentin <vasnake@gmail.com>
2019-04-23 17:40:07 +0300

    fixed wrapper skeleton

--- src/main/python/me/valik/spark/transformer.py
+++ src/main/python/me/valik/spark/transformer.py
@@ -6,30 +6,30 @@ from pyspark.ml.param.shared import *
 from pyspark.ml.util import JavaMLReadable, JavaMLWritable
 from pyspark.ml.wrapper import JavaTransformer
 
+# from pyspark.ml.feature import SQLTransformer
+
 
 @inherit_doc
 class BroadcastSpatialJoin(JavaTransformer, JavaMLReadable, JavaMLWritable):
-    predicate = Param(Params._dummy(), "predicate", "spatial op, one of: withindist, within, contains, intersects, overlaps, nearest")
-    fqdn = "me.valik.spark.transformer.BroadcastSpatialJoin"
+    _fqdn = "me.valik.spark.transformer.BroadcastSpatialJoin"
+    predicate = Param(Params._dummy(), "predicate", "spatial op, one of: withindist, within, contains, intersects, overlaps, nearest",
+                      typeConverter=TypeConverters.toString)
 
     @keyword_only
-    def __init__(self, uid=None, predicate = "nearest"):
+    def __init__(self, predicate="nearest"):
         super(BroadcastSpatialJoin, self).__init__()
-
-        if uid:
-            self._java_obj = self._new_java_obj(self.fqdn, uid)
-        else:
-            self._java_obj = self._new_java_obj(self.fqdn)
+        self._java_obj = self._new_java_obj(self._fqdn, self.uid)
 
         # set params default values
         self._setDefault(predicate="nearest")
+
         # set params init values
-        kwargs = {"predicate": predicate}
+        kwargs = self._input_kwargs
         self.setParams(**kwargs)
 
     @keyword_only
     def setParams(self, predicate="nearest"):
-        kwargs = {"predicate": predicate}
+        kwargs = self._input_kwargs
         return self._set(**kwargs)
 
     def setPredicate(self, value):
